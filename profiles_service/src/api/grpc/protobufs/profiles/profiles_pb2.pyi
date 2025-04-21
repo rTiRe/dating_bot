dafab400491cdabbaf358f65d477e0d1ccad5340
roganofff <matematic1337@gmail.com>
2025-04-21 18:42:57 +0300

    feat: added Create, Update and Delete methods

--- profiles_service/src/api/grpc/protobufs/profiles/profiles_pb2.pyi
+++ profiles_service/src/api/grpc/protobufs/profiles/profiles_pb2.pyi
@@ -1,26 +1,20 @@
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class Gender(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     __slots__ = ()
+    GENDER_DEFAULT: _ClassVar[Gender]
     GENDER_MALE: _ClassVar[Gender]
     GENDER_FEMALE: _ClassVar[Gender]
+GENDER_DEFAULT: Gender
 GENDER_MALE: Gender
 GENDER_FEMALE: Gender
 
-class ProfilesGetRequest(_message.Message):
-    __slots__ = ("id", "account_id")
-    ID_FIELD_NUMBER: _ClassVar[int]
-    ACCOUNT_ID_FIELD_NUMBER: _ClassVar[int]
-    id: str
-    account_id: str
-    def __init__(self, id: _Optional[str] = ..., account_id: _Optional[str] = ...) -> None: ...
-
-class ProfilesGetResponse(_message.Message):
+class ProfileCreateRequest(_message.Message):
     __slots__ = ("id", "account_id", "first_name", "last_name", "age", "gender", "biography", "additional_info")
     ID_FIELD_NUMBER: _ClassVar[int]
     ACCOUNT_ID_FIELD_NUMBER: _ClassVar[int]
@@ -39,3 +33,114 @@ class ProfilesGetResponse(_message.Message):
     biography: str
     additional_info: str
     def __init__(self, id: _Optional[str] = ..., account_id: _Optional[str] = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., age: _Optional[int] = ..., gender: _Optional[_Union[Gender, str]] = ..., biography: _Optional[str] = ..., additional_info: _Optional[str] = ...) -> None: ...
+
+class ProfilesGetRequest(_message.Message):
+    __slots__ = ("id", "account_id", "first_name", "last_name", "age", "gender", "biography", "additional_info")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    ACCOUNT_ID_FIELD_NUMBER: _ClassVar[int]
+    FIRST_NAME_FIELD_NUMBER: _ClassVar[int]
+    LAST_NAME_FIELD_NUMBER: _ClassVar[int]
+    AGE_FIELD_NUMBER: _ClassVar[int]
+    GENDER_FIELD_NUMBER: _ClassVar[int]
+    BIOGRAPHY_FIELD_NUMBER: _ClassVar[int]
+    ADDITIONAL_INFO_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    account_id: int
+    first_name: str
+    last_name: str
+    age: int
+    gender: Gender
+    biography: str
+    additional_info: str
+    def __init__(self, id: _Optional[str] = ..., account_id: _Optional[int] = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., age: _Optional[int] = ..., gender: _Optional[_Union[Gender, str]] = ..., biography: _Optional[str] = ..., additional_info: _Optional[str] = ...) -> None: ...
+
+class ProfileUpdateRequest(_message.Message):
+    __slots__ = ("id", "data")
+    class UpdateData(_message.Message):
+        __slots__ = ("account_id", "first_name", "last_name", "age", "gender", "biography", "additional_info")
+        ACCOUNT_ID_FIELD_NUMBER: _ClassVar[int]
+        FIRST_NAME_FIELD_NUMBER: _ClassVar[int]
+        LAST_NAME_FIELD_NUMBER: _ClassVar[int]
+        AGE_FIELD_NUMBER: _ClassVar[int]
+        GENDER_FIELD_NUMBER: _ClassVar[int]
+        BIOGRAPHY_FIELD_NUMBER: _ClassVar[int]
+        ADDITIONAL_INFO_FIELD_NUMBER: _ClassVar[int]
+        account_id: str
+        first_name: str
+        last_name: str
+        age: int
+        gender: Gender
+        biography: str
+        additional_info: str
+        def __init__(self, account_id: _Optional[str] = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., age: _Optional[int] = ..., gender: _Optional[_Union[Gender, str]] = ..., biography: _Optional[str] = ..., additional_info: _Optional[str] = ...) -> None: ...
+    ID_FIELD_NUMBER: _ClassVar[int]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    data: ProfileUpdateRequest.UpdateData
+    def __init__(self, id: _Optional[str] = ..., data: _Optional[_Union[ProfileUpdateRequest.UpdateData, _Mapping]] = ...) -> None: ...
+
+class ProfileDeleteRequest(_message.Message):
+    __slots__ = ("id",)
+    ID_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    def __init__(self, id: _Optional[str] = ...) -> None: ...
+
+class ProfileCreateResponse(_message.Message):
+    __slots__ = ("id", "account_id", "first_name", "last_name", "age", "gender", "biography", "additional_info", "created_at", "updated_at")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    ACCOUNT_ID_FIELD_NUMBER: _ClassVar[int]
+    FIRST_NAME_FIELD_NUMBER: _ClassVar[int]
+    LAST_NAME_FIELD_NUMBER: _ClassVar[int]
+    AGE_FIELD_NUMBER: _ClassVar[int]
+    GENDER_FIELD_NUMBER: _ClassVar[int]
+    BIOGRAPHY_FIELD_NUMBER: _ClassVar[int]
+    ADDITIONAL_INFO_FIELD_NUMBER: _ClassVar[int]
+    CREATED_AT_FIELD_NUMBER: _ClassVar[int]
+    UPDATED_AT_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    account_id: str
+    first_name: str
+    last_name: str
+    age: int
+    gender: Gender
+    biography: str
+    additional_info: str
+    created_at: str
+    updated_at: str
+    def __init__(self, id: _Optional[str] = ..., account_id: _Optional[str] = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., age: _Optional[int] = ..., gender: _Optional[_Union[Gender, str]] = ..., biography: _Optional[str] = ..., additional_info: _Optional[str] = ..., created_at: _Optional[str] = ..., updated_at: _Optional[str] = ...) -> None: ...
+
+class ProfilesGetResponse(_message.Message):
+    __slots__ = ("id", "account_id", "first_name", "last_name", "age", "gender", "biography", "additional_info", "created_at", "updated_at")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    ACCOUNT_ID_FIELD_NUMBER: _ClassVar[int]
+    FIRST_NAME_FIELD_NUMBER: _ClassVar[int]
+    LAST_NAME_FIELD_NUMBER: _ClassVar[int]
+    AGE_FIELD_NUMBER: _ClassVar[int]
+    GENDER_FIELD_NUMBER: _ClassVar[int]
+    BIOGRAPHY_FIELD_NUMBER: _ClassVar[int]
+    ADDITIONAL_INFO_FIELD_NUMBER: _ClassVar[int]
+    CREATED_AT_FIELD_NUMBER: _ClassVar[int]
+    UPDATED_AT_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    account_id: str
+    first_name: str
+    last_name: str
+    age: int
+    gender: Gender
+    biography: str
+    additional_info: str
+    created_at: str
+    updated_at: str
+    def __init__(self, id: _Optional[str] = ..., account_id: _Optional[str] = ..., first_name: _Optional[str] = ..., last_name: _Optional[str] = ..., age: _Optional[int] = ..., gender: _Optional[_Union[Gender, str]] = ..., biography: _Optional[str] = ..., additional_info: _Optional[str] = ..., created_at: _Optional[str] = ..., updated_at: _Optional[str] = ...) -> None: ...
+
+class ProfilesUpdateResponse(_message.Message):
+    __slots__ = ("result",)
+    RESULT_FIELD_NUMBER: _ClassVar[int]
+    result: str
+    def __init__(self, result: _Optional[str] = ...) -> None: ...
+
+class ProfilesDeleteResponse(_message.Message):
+    __slots__ = ("result",)
+    RESULT_FIELD_NUMBER: _ClassVar[int]
+    result: str
+    def __init__(self, result: _Optional[str] = ...) -> None: ...
--- profiles_service/src/api/grpc/protobufs/profiles/profiles_pb2_grpc.py
+++ profiles_service/src/api/grpc/protobufs/profiles/profiles_pb2_grpc.py
@@ -34,6 +34,11 @@ class ProfilesServiceStub(object):
         Args:
             channel: A grpc.Channel.
         """
+        self.Create = channel.unary_unary(
+                '/dating.service.profiles.ProfilesService/Create',
+                request_serializer=profiles__pb2.ProfileCreateRequest.SerializeToString,
+                response_deserializer=profiles__pb2.ProfileCreateResponse.FromString,
+                _registered_method=True)
         self.Get = channel.unary_unary(
                 '/dating.service.profiles.ProfilesService/Get',
                 request_serializer=profiles__pb2.ProfilesGetRequest.SerializeToString,
@@ -44,6 +49,12 @@ class ProfilesServiceStub(object):
 class ProfilesServiceServicer(object):
     """Missing associated documentation comment in .proto file."""
 
+    def Create(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
     def Get(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
@@ -53,6 +64,11 @@ class ProfilesServiceServicer(object):
 
 def add_ProfilesServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
+            'Create': grpc.unary_unary_rpc_method_handler(
+                    servicer.Create,
+                    request_deserializer=profiles__pb2.ProfileCreateRequest.FromString,
+                    response_serializer=profiles__pb2.ProfileCreateResponse.SerializeToString,
+            ),
             'Get': grpc.unary_unary_rpc_method_handler(
                     servicer.Get,
                     request_deserializer=profiles__pb2.ProfilesGetRequest.FromString,
@@ -68,6 +84,33 @@ def add_ProfilesServiceServicer_to_server(servicer, server):
  # This class is part of an EXPERIMENTAL API.
 class ProfilesService(object):
     """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def Create(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/dating.service.profiles.ProfilesService/Create',
+            profiles__pb2.ProfileCreateRequest.SerializeToString,
+            profiles__pb2.ProfileCreateResponse.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
 
     @staticmethod
     def Get(request,
