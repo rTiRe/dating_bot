c4b904fcaa2a994b8ef62018008ed9a5391a1742
roganofff <matematic1337@gmail.com>
2025-04-23 05:07:46 +0300

    fix: added gender handling to repository

--- profiles_service/src/repositories/profiles.py
+++ profiles_service/src/repositories/profiles.py
@@ -1,6 +1,8 @@
+from asyncpg import Record
 from asyncpg.connection import Connection
 from pydantic import BaseModel
 
+from src.exceptions import UpdateAllRowsException
 from src.repositories.base import BaseRepository
 from src.schemas.profile import CreateProfileSchema, ProfileSchema, UpdateProfileSchema
 from src.specifications.base import Specification
@@ -12,12 +14,19 @@ class ProfilesRepository(BaseRepository):
         connection: Connection,
         create_data: CreateProfileSchema,
     ) -> ProfileSchema:
-        return await super(__class__, __class__).create(
-            connection,
-            'dating.profiles',
-            ProfileSchema,
-            create_data,
-        )
+        data_dict = create_data.model_dump()
+        data_dict['gender'] = str(data_dict['gender'].value)
+        statement_values = []
+        columns = ', '.join([column for column in data_dict.keys()])
+        values_pattern = ', '.join('??' for _ in range(len(data_dict)))
+        statement_values.extend(list(data_dict.values()))
+        statement = f"""
+            insert into dating.profiles ({columns}) values ({values_pattern})
+            returning *
+        """
+        statement = await Specification.to_asyncpg_query(f'{statement};')
+        model_data: Record = await connection.fetchrow(statement, *statement_values)
+        return ProfileSchema(**model_data)
 
     @staticmethod
     async def get(
@@ -42,13 +51,25 @@ class ProfilesRepository(BaseRepository):
             update_all: bool = False,
             update_data: UpdateProfileSchema,
     ) -> str:
-        return await super(__class__, __class__).update(
-            connection,
-            'dating.profiles',
-            *specifications,
-            update_all=update_all,
-            update_data=update_data,
-        )
+        if not specifications and not update_all:
+            raise UpdateAllRowsException(
+                f'You are trying to update all rows from a table dating.profiles. '
+                'If you are sure, set update_all to True.'
+            )
+        update_values = update_data.model_dump(exclude_unset=True)
+        if update_values.get('gender'):
+            update_values['gender'] = str(update_values['gender'].value)
+        set_statement = ', '.join([f'{column} = ??' for column in update_values.keys()])
+        statement_values = list(update_values.values())
+        statement = f'update dating.profiles set {set_statement}'
+        if specifications:
+            statement, statement_values = await BaseRepository.add_conditions(
+                statement,
+                statement_values,
+                *specifications,
+            )
+        statement = await Specification.to_asyncpg_query(f'{statement};')
+        return await connection.execute(statement, *statement_values)
 
     @staticmethod
     async def delete(
